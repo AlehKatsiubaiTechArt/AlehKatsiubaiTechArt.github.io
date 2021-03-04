b43bdb72cfe555abf500f11fbacd9f63a4ac7d6d
AlehKatsiuba <oleg@form.io>
2021-03-04 03:35:25 +0300

    build

--- src/slides/renderer.tsx
+++
@@ -1,7 +0,0 @@
-import { Html } from '@react-three/drei'
-import React from 'react'
-import CanvasWithTitle from '../layouts/CanvasWithTitle'
-
-export default function App() {
-  return <CanvasWithTitle title="Renderer" />
-}
--- src/types.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      axisHelper: object
-    }
-  }
-}
-
-export default {}
